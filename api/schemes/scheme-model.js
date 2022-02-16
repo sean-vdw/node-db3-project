b5e285b010730b835e591827aa686dc75613be22
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-16 14:01:09 -0800

    MVP completed

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -163,6 +163,13 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+  return db('steps').insert({
+    ...step,
+    scheme_id
+  })
+  .then(() => {
+    return db('steps').where('scheme_id', scheme_id).orderBy('step_number');
+  });
 }
 
 module.exports = {
